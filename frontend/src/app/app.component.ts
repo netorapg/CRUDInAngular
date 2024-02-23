ab5fae25597fc6e79b1a22a35f520189ad5fbb12
netorapg <renatoplatz11@outlook.com>
2024-02-23 16:58:24 -0300

    criando o rodapé

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/template/header/header.component";
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { FooterComponent } from "./components/template/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: 'app.component.html',
-    imports: [RouterOutlet, HeaderComponent, MatToolbarModule]
+    imports: [RouterOutlet, HeaderComponent, MatToolbarModule, FooterComponent]
 })
 export class AppComponent {
  
