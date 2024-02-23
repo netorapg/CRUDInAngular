31dee649b7fe232e1af952bae0844c6555e01101
netorapg <renatoplatz11@outlook.com>
2024-02-23 15:13:40 -0300

    trabalhando no cabeçalho. Adicionado um logo

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/template/header/header.component";
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: 'app.component.html',
-    imports: [RouterOutlet, HeaderComponent]
+    imports: [RouterOutlet, HeaderComponent, MatToolbarModule]
 })
 export class AppComponent {
  
