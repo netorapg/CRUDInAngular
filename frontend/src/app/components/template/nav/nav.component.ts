49de3e2c121b206acb56a6e444b901f399203d4a
netorapg <renatoplatz11@outlook.com>
2024-02-24 12:54:43 -0300

    trabalhando na tela de inicio

--- frontend/src/app/components/template/nav/nav.component.ts
+++ frontend/src/app/components/template/nav/nav.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { MatSidenavContainer } from '@angular/material/sidenav';
 import { MatSidenav } from '@angular/material/sidenav';
 import { MatNavList } from '@angular/material/list';
+import { MatSidenavContent } from '@angular/material/sidenav';
+import { HomeComponent } from "../../../views/home/home.component";
 
 @Component({
-  selector: 'app-nav',
-  standalone: true,
-  imports: [MatSidenavContainer, MatSidenav, MatNavList],
-  templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+    selector: 'app-nav',
+    standalone: true,
+    templateUrl: './nav.component.html',
+    styleUrl: './nav.component.css',
+    imports: [MatSidenavContainer, MatSidenav, MatNavList, MatSidenavContent, HomeComponent]
 })
 export class NavComponent {
 
