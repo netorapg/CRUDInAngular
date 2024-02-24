f4c3e136ea26c010c7d8b443fae616fc0f7365ce
netorapg <renatoplatz11@outlook.com>
2024-02-24 13:02:51 -0300

    trabalhando na tela Inicio

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/template/header/header.component";
-
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { NavComponent } from './components/template/nav/nav.component';
-
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './views/home/home.component';
 import {} from '@angular/material/list';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: 'app.component.html',
-    imports: [RouterOutlet, HeaderComponent, MatToolbarModule, FooterComponent, NavComponent, MatSidenavModule]
+    imports: [RouterOutlet, HeaderComponent, 
+        MatToolbarModule, FooterComponent, 
+        NavComponent, MatSidenavModule, 
+        HomeComponent, MatCardModule]
 })
 export class AppComponent {
  
